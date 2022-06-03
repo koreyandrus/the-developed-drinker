9fb939af99246cf6c2d4b14b2974559fefb1fbe5
Korey Andrus <korey86@gmail.com>
2022-06-02 19:46:58 -0500

    ADD: API service untested

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
+  private url: string = 'www.thecocktaildb.com/api/json/v1/1/';
 
-  constructor() { }
+  getRandomCocktail() {
+    return this.http.get(this.url + 'random.php');
+  }
+
+  searchCocktailName(name: string) {
+    return this.http.get(this.url + 'search.php?s=' + name);
+  }
+
+  searchIngredientName(name: string) {
+    return this.http.get(this.url + 'search.php?i=' + name);
+  }
 }
