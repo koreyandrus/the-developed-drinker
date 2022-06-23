2297f91aa4cdf62e67f822117580266f934afa11
Korey Andrus <korey86@gmail.com>
2022-06-22 22:12:53 -0500

    Work on UI update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  menu: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showMenu() {
+    this.menu = !this.menu;
+    console.log(this.menu);
   }
-
 }
